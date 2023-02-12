0e3124cc211ea862ebb2597486a1398e0593b68f
Luke Bakken <luke@bakken.io>
2023-02-12 07:59:21 -0800

    Continue simplifying the code

--- broker/mosquitto/mosquitto_server.sh
+++
@@ -1,9 +0,0 @@
-# ubuntu
-#apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
-
-apt-get update -y
-apt-get install mosquitto -y
-
-# status
-# service mosquitto status
-# systemctl status mosquitto
